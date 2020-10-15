51c69f4f38e7d10b7103a466eee0cae2df28955c
kevinlucas23 <54790615+kevinlucas23@users.noreply.github.com>
2020-10-15 03:55:16 -0400

    k

--- user_project3.c
+++ user_project3.c
@@ -9,7 +9,7 @@ void delay(int secs)
 	while (clock() < start_time + milli_seconds);
 }
 
-static void to_read(unsigned long k)
+void to_read(unsigned long k)
 {
 	char user_i[20], *c;
 	unsigned long num, i = 0;
@@ -145,7 +145,7 @@ void* socket_handler_thread(void* arg)
 	return (void*)0;
 }
 
-static void all_pages()
+void all_pages()
 {
 	int j;
 	for (j = 0; j < 100; ++j) {
