9bc10da11ced49619e4bac65928ae8a590e5824b
kevinlucas23 <54790615+kevinlucas23@users.noreply.github.com>
2020-10-16 19:50:14 -0400

    Part 3 done

--- main_p3.c
+++ main_p3.c
@@ -45,7 +45,7 @@ int main(int argc, char* argv[])
 			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	}
 
-	//fault_region(&k, &handl, &thr);
+	fault_region(&k, &handl, &thr);
 	assign_addr_to_pages((uint64_t)k.mmap_addr);
 
 	for (;;) {
