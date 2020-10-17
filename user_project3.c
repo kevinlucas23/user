5dc133b506b4e50f19eefc94fd2a0ad681df4ad4
kevinlucas23 <54790615+kevinlucas23@users.noreply.github.com>
2020-10-16 23:20:05 -0400

    k

--- user_project3.c
+++ user_project3.c
@@ -23,8 +23,7 @@ void to_read()
 
 	if ((int)num == -1) {
 		for (;;) {
-			 printf("%lu, pages n %d\n", i, (int)num_pages);
-			 if (i == num_pages)
+			if (i == num_pages)
 				 break;
 			c = (char*)all_page[(int)i].mmap_addr;
 			if (c == NULL) {
