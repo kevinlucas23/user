2ee624e999454db4a40e5bdaa159f152da7cf284
kevinlucas23 <54790615+kevinlucas23@users.noreply.github.com>
2020-10-23 20:23:34 -0400

    Part 4 starting

--- user_project3.c
+++ user_project3.c
@@ -212,7 +212,7 @@ long fault_region(struct mmap_info* k, void** start_handle, pthread_t* thr)
 	return uffd;
 }
 
-void thread_socket_handler(void* arg) {
+void* thread_socket_handler(void* arg) {
 	int sk = *(int*)arg;
 	/* Ensure it's not stdin/out/err */
 	printf("Cleanup handler called: %d\n", sk);
@@ -254,7 +254,7 @@ void* thread_socket(void* arg) {
 			errExit("Unable to read in socket thread");
 		}
 	}
-	//pthread_cleanup_pop(0);
+	pthread_cleanup_pop(0);
 	return NULL;
 }
 
