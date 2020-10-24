bd2bac099d8c5ffec3cfb0ec4a3321a72eca785f
kevinlucas23 <54790615+kevinlucas23@users.noreply.github.com>
2020-10-23 20:43:20 -0400

    Part 4 starting

--- main_p3.c
+++ main_p3.c
@@ -72,7 +72,7 @@ int main(int argc, char* argv[])
 			pthread_cancel(soc_thr);
 			printf("int here\n");
 			kev.a_mess = end_erything;
-			if (write(out, &kev, sizeof(kev)) <= 0) {
+			if (write(out, &kev, sizeof(kev)) < 0) {
 				printf("closing socket: %i", out);
 				errExit("Error exiting the thread");
 			}
